93bb91b85c5c1bf9dd9110b6d5eaa98be73bc409
cog-pedro-galhardi <pedro.galhardi@cognitivo.ai>
2024-09-27 15:48:07 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,73 +1,43 @@
 import streamlit as st
 import pandas as pd
 
-st.markdown("""
-    <style>
-        .circle {
-            height: 15px;
-            width: 15px;
-            border-radius: 50%;
-            display: inline-block;
-            margin-right: 15px;  
-        }
-        .red { background-color: red; }
-        .yellow { background-color: yellow; }
-        .green { background-color: green; }
-        .circle-container {
-            position: absolute;
-            display: flex; 
-        }
-        .logo {
-            position: fixed; 
-            bottom: 20px; 
-            left: 20px; 
-            width: 50px; 
-            height: auto; 
-        }
-
-    </style>
-    <div class='circle-container'>
-        <div class='circle red'></div>
-        <div class='circle yellow'></div>
-        <div class='circle green'></div>
-    </div>
-
-""", unsafe_allow_html=True)
-
+# Titulo 
 st.markdown("<h1 style='color: blue;'><strong>Smart Mix</strong></h1>", unsafe_allow_html=True)
-
-
 st.markdown("<h4 style='color: blue;'><strong>Informe a localização informada da loja a ser aberta:(Brick, Utc)</strong></h4>", unsafe_allow_html=True)
 
+# Colunas e Imputs Labels
 col1, col2, col3, col4 = st.columns(4)
 
 with col1:
-    brick = st.text_input("", placeholder="Brick", label_visibility='hidden')
-    st.write(brick)
+    brick = st.text_input("Preencha com o Brick", placeholder="Brick")
 
 with col2:
-    utc = st.text_input("", placeholder="Utc", label_visibility='hidden')
-    st.write(utc)
-    
+    utc = st.text_input("Preencha com o Utc", placeholder="Utc")
+  
 with col3:
     tamLoja = st.selectbox(
-    "Tamanho de sua loja",
-    ("P", "M", "G", "GG"),
-    index=None,
-    placeholder="Tamanho da loja",
-)
+        "Tamanho de sua loja",
+        ("P", "M", "G", "GG"),
+        index=None,
+        placeholder="Tamanho da loja",
+    )
 
-st.write("O tamanho da loja selecionada é:", tamLoja) 
-    
 with col4:
     regiao = st.selectbox(
-    "Selecionar sua Região",
-    ("Norte", "Nordeste", "Centro-Oeste", "Sudeste", "Sul"),
-    index=None,
-    placeholder="Sua região",
-)
+        "Selecionar sua Região:",
+        ("Norte", "Nordeste", "Centro-Oeste", "Sudeste", "Sul"),
+        index=None,
+        placeholder="Sua região",
+    )
 
-st.write("A sua região selecionada é:", regiao)  
+# Funcao para poder armazenas as variaveis
+def processar_dados(brick, utc, tamLoja, regiao):
+
+    st.write("Dados armazenados:")
+    st.write(f"Brick: {brick}, UTC: {utc}, Tamanho da Loja: {tamLoja}, Região: {regiao}")
+    
 
 
-st.markdown("<img class='logo' src='logo.png' alt='Logo'>", unsafe_allow_html=True)
+# Botao para o processamento de dados
+if st.button('Confirmar e Processar Dados'):
+    processar_dados(brick, utc, tamLoja, regiao)
