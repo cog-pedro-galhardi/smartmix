7b7fed1a6a12c06c5feb2f0a8e7603a97adb3e0a
cog-pedro-galhardi <pedro.galhardi@cognitivo.ai>
2024-10-14 18:03:32 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,17 +5,11 @@ import io
 import os
 from dotenv import load_dotenv
 
-st.markdown(
-    "<h1 style='color: blue;'><strong>Smart Mix</strong></h1>", unsafe_allow_html=True
-)
-st.markdown(
-    "<h4 style='color: blue;'><strong>Informe a localização informada da loja a ser aberta:(Brick, Utc)</strong></h4>",
-    unsafe_allow_html=True,
-)
 
 st.markdown(
     """
     <style>
+
     div.stButton > button {
         background-color: #007BFF;
         color: white;
@@ -42,17 +36,22 @@ st.markdown(
         background-color: #0056b3;
         color: white;
     }
+
     </style>
     """,
     unsafe_allow_html=True,
 )
 
+st.markdown('<div class="main-container">', unsafe_allow_html=True)
 
-# Mock de inputs
-# brick_input = 1032
-# utc_input = 1500404000
-# tamLoja_input = "G"
-# regiao_input = "Região Sul"
+st.markdown(
+    "<h1 style='color: blue;'><strong>Smart Mix</strong></h1>", unsafe_allow_html=True
+)
+st.markdown(
+    "<h4 style='color: blue;'><strong>Informe a localização informada da loja a ser aberta:</strong></h4>",
+    unsafe_allow_html=True,
+)
+
 
 col1, col2, col3, col4 = st.columns(4)
 
@@ -73,8 +72,6 @@ with col3:
         index=None,
         placeholder="Tamanho da loja",
     )
-    
-
 
 with col4:
     regiao_input = st.selectbox(
@@ -90,7 +87,13 @@ with col4:
         placeholder="Sua região",
     )
 
-st.write(brick_input, utc_input, tamLoja_input, regiao_input)
+# Mock de inputs
+# brick_input = 1032
+# utc_input = 1500404000
+# tamLoja_input = "G"
+# regiao_input = "Região Sul"
+
+#st.write(brick_input, utc_input, tamLoja_input, regiao_input)
 
 if st.button("Processamento dos dados"):
 
@@ -104,47 +107,53 @@ if st.button("Processamento dos dados"):
         "fato_pcp": "fato_pcp_cubo_farmarcas_iqvia_202409192107.csv",
         "cadastro_produtos": "cadastro_produtos_iqvia_202409192057.csv",
     }
+    progress = st.progress(0)
+
 
     # conectando com a AWS
     load_dotenv()
     session = boto3.Session(
-        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
         region_name="us-east-2",
     )
     s3 = session.client("s3")
 
     # funcao para download e leitura de arquivos do S3
     def baixar_arquivo_s3(nome_arquivo, tipo="excel"):
-        st.write(f"Baixando arquivo: {nome_arquivo}...")
         try:
             obj = s3.get_object(Bucket=bucket_name, Key=nome_arquivo)
             content = obj["Body"].read()
-            st.write(f"Arquivo {nome_arquivo} baixado com sucesso!")
 
             if tipo == "excel":
                 return pd.read_excel(io.BytesIO(content))
+                progress.progress(1 / total_etapas)
+
             elif tipo == "csv":
                 return pd.read_csv(
                     io.StringIO(content.decode("utf-8")), sep=";", low_memory=False
                 )
+                progress.progress(2 / total_etapas)
+
         except Exception as e:
             st.error(f"Erro ao baixar {nome_arquivo}: {e}")
             return None
+    total_etapas = 9
+    progress.progress(3 / total_etapas)
 
     # baixar e carregar os dados do bucket
     perfil_lojas = baixar_arquivo_s3(arquivos_s3["perfil_lojas"])
     iqvia_med = baixar_arquivo_s3(arquivos_s3["iqvia_med"])
     iqvia_naomed = baixar_arquivo_s3(arquivos_s3["iqvia_naomed"])
     iqvia_consolidado = pd.concat([iqvia_med, iqvia_naomed])
-
     close_up = baixar_arquivo_s3(arquivos_s3["close_up"], tipo="csv")
     sellout_julho = baixar_arquivo_s3(arquivos_s3["sellout_julho"], tipo="csv")
     fato_pcp = baixar_arquivo_s3(arquivos_s3["fato_pcp"], tipo="csv")
     cadastro_produtos = baixar_arquivo_s3(arquivos_s3["cadastro_produtos"], tipo="csv")
 
+    progress.progress(4 / total_etapas)
+
     # ajustar as categorias no cadastro de produtos
-    st.write("Ajustando categorias no cadastro de produtos.")
     cadastro_produtos["categoria_ajustada"] = cadastro_produtos.apply(
         lambda row: (
             row["classe_1"]
@@ -153,7 +162,8 @@ if st.button("Processamento dos dados"):
         ),
         axis=1,
     )
-    st.write("Categorias ajustadas com sucesso!")
+
+    progress.progress(5 / total_etapas)
 
     # classificação Pareto
     def pareto_classification(row):
@@ -161,12 +171,14 @@ if st.button("Processamento dos dados"):
             return "A"
         elif row["cumulative_percentage"] <= 80:
             return "B"
-        else:
+        elif row["cumulative_percentage"] <= 90:
             return "C"
+        else:
+            return "D"
+    progress.progress(6 / total_etapas)  
 
     # gerar a classificação Pareto
     def df_pareto_func(df, col1, col2, col_unid):
-        st.write("Gerando classificação Pareto.")
 
         pareto_out = df.groupby([col1, col2, "ean"])[col_unid].sum().reset_index()
 
@@ -191,18 +203,24 @@ if st.button("Processamento dos dados"):
             pareto_classification, axis=1
         )
 
-        st.write("Classificação Pareto gerada com sucesso!")
         return pareto_out
 
+    progress.progress(7 / total_etapas)
+
     # feiltrar os perfis por tamanho e regiao
-    st.write("Filtrando perfil de lojas...")
     perfil_lojas_filtrado = perfil_lojas[
         (perfil_lojas["tamanho_sigla"] == tamLoja_input)
         & (perfil_lojas["Região"] == regiao_input)
     ]
+
+    colunas_para_excluir = ['CD_GEOCODI', 'CD_GEOCODM', 'layer', 'fid']  # Exemplo de colunas
+    perfil_lojas_filtrado = perfil_lojas_filtrado.drop(columns=colunas_para_excluir)
+
+    # Exibir o número de registros filtrados
     st.write(
         f"Perfil de lojas filtrado: {len(perfil_lojas_filtrado)} registros encontrados."
     )
+    st.dataframe(perfil_lojas_filtrado)
 
     # verificar se o data frame esta vazio
     if perfil_lojas_filtrado.empty:
@@ -210,32 +228,6 @@ if st.button("Processamento dos dados"):
             "Nenhuma loja encontrada com o filtro aplicado (tamanho e região). Verifique os inputs."
         )
     else:
-        st.write(perfil_lojas_filtrado.head())
-    st.write("Fazendo merge de classe social com cadastro de produtos...")
-
-    try:
-        # verificar se os DataFrames tem dados
-        st.write(f"Tamanho do DataFrame 'sellout_julho': {sellout_julho.shape}")
-        st.write(f"Tamanho do DataFrame 'cadastro_produtos': {cadastro_produtos.shape}")
-        st.write(
-            f"Tamanho do DataFrame 'perfil_lojas_filtrado': {perfil_lojas_filtrado.shape}"
-        )
-
-        # verificar se as colunas de merge existem dados
-        st.write(
-            f"Checando valores nulos em 'sellout_julho' para 'cod_barras': {sellout_julho['cod_barras'].isnull().sum()}"
-        )
-        st.write(
-            f"Checando valores nulos em 'cadastro_produtos' para 'ean': {cadastro_produtos['ean'].isnull().sum()}"
-        )
-        st.write(
-            f"Checando valores nulos em 'sellout_julho' para 'cnpj': {sellout_julho['cnpj'].isnull().sum()}"
-        )
-        st.write(
-            f"Checando valores nulos em 'perfil_lojas_filtrado' para 'CNPJ': {perfil_lojas_filtrado['CNPJ'].isnull().sum()}"
-        )
-
-        # merge entre 'sellout_julho' e 'cadastro_produtos'
         merge_classe_categoria = sellout_julho.merge(
             cadastro_produtos, left_on="cod_barras", right_on="ean", how="left"
         )
@@ -244,51 +236,29 @@ if st.button("Processamento dos dados"):
         merge_classe_categoria = merge_classe_categoria.merge(
             perfil_lojas_filtrado, left_on="cnpj", right_on="CNPJ", how="left"
         )
+        (merge_classe_categoria[["cod_barras", "ean"]].drop_duplicates())
 
-        # tirar as colunas null do EAN
-        ean_none_count = merge_classe_categoria["ean"].isnull().sum()
-        if ean_none_count > 0:
-            merge_classe_categoria = merge_classe_categoria[
-                merge_classe_categoria["ean"].notnull()
-            ]
-            st.write(
-                f"Linhas com EANs None removidas. Total de linhas restantes: {merge_classe_categoria.shape[0]}"
+        # juntar tamanho e loja em uma coluna
+        try:
+            merge_classe_categoria["uf_tam"] = (
+                merge_classe_categoria["Região"]
+                + merge_classe_categoria["tamanho_sigla"]
             )
-        else:
-            st.write("Nenhum EAN None encontrado.")
+        except Exception as e:
+            st.error(f"Erro ao combinar UF e tamanho da loja: {e}")
 
-        st.write("Merge de classe social realizado com sucesso!")
-    except Exception as e:
-        st.error(f"Erro ao fazer merge de classe social: {e}")
+        # classificação pareto no dataFrame filtrado por regiao e tamanho
+        try:
+            df_classe_categoria = df_pareto_func(
+                merge_classe_categoria, "uf_tam", "categoria_ajustada", "sum_unidade"
+            )
+        except Exception as e:
+            st.error(
+                f"Erro ao aplicar classificação Pareto no DataFrame de classe social: {e}"
+            )
 
-    st.write("EAN no primeiro merge:")
-    st.write(merge_classe_categoria[["cod_barras", "ean"]].drop_duplicates())
-
-    # juntar tamanho e loja em uma coluna
-    try:
-        merge_classe_categoria["uf_tam"] = (
-            merge_classe_categoria["Região"] + merge_classe_categoria["tamanho_sigla"]
-        )
-        st.write("Coluna UF e tamanho da loja combinada com sucesso!")
-    except Exception as e:
-        st.error(f"Erro ao combinar UF e tamanho da loja: {e}")
-
-    # classificacao pareto no dataFrame filtrado por regiao e tamanho
-    st.write("Aplicando classificação Pareto no DataFrame de classe social")
-    try:
-        df_classe_categoria = df_pareto_func(
-            merge_classe_categoria, "uf_tam", "categoria_ajustada", "sum_unidade"
-        )
-        st.write("Classificação Pareto aplicada com sucesso!")
-    except Exception as e:
-        st.error(
-            f"Erro ao aplicar classificação Pareto no DataFrame de classe social: {e}"
-        )
-
-    st.dataframe(df_classe_categoria)
 
     # filtro de brick e utc
-    st.write("Filtrando Brick e UTC")
     merge_brick_categoria = fato_pcp.merge(
         cadastro_produtos, left_on="ean", right_on="ean", how="left"
     )
@@ -308,43 +278,82 @@ if st.button("Processamento dos dados"):
         df_utc_categoria_filtered = df_utc_categoria[
             df_utc_categoria["UTC"] == utc_input
         ]
-        st.write("Filtragem de Brick e UTC concluída com sucesso!")
     except Exception as e:
         st.error(f"Erro ao filtrar Brick e UTC: {e}")
-
-    st.dataframe(cadastro_produtos)
     # juntar os dataFrames
-    st.write("Combinando DataFrames filtrados")
     try:
-        merged_df = df_brick_categoria_filtered.merge(
-            df_utc_categoria_filtered,
-            on="ean",
-            how="inner",
-            suffixes=("_brick", "_utc")
-        ).merge(
-            df_classe_categoria, on="ean", how="inner", suffixes=("_merged", "_classe")
-        ).merge(
-            cadastro_produtos[['ean', 'produto', 'categoria_ajustada']], 
-            on="ean", 
-            how="left"
+        merged_df = (
+            df_brick_categoria_filtered.merge(
+                df_utc_categoria_filtered,
+                on="ean",
+                how="outer",
+                suffixes=("_brick", "_utc"),
+            )
+            .merge(
+                df_classe_categoria,
+                on="ean",
+                how="outer",
+                suffixes=("_merged", "_classe"),
+            )
+            .merge(
+                cadastro_produtos[["ean", "produto", "categoria_ajustada"]],
+                on="ean",
+                how="outer",
+            )
         )
-        merged_df['categoria_ajustada'] = merged_df['categoria_ajustada_brick'].combine_first(
-            merged_df['categoria_ajustada_utc']
-        ).combine_first(
-            merged_df['categoria_ajustada_x']
-        ).combine_first(
-            merged_df['categoria_ajustada_y']
+        merged_df["categoria_ajustada"] = (
+            merged_df["categoria_ajustada_brick"]
+            .combine_first(merged_df["categoria_ajustada_utc"])
+            .combine_first(merged_df["categoria_ajustada_x"])
+            .combine_first(merged_df["categoria_ajustada_y"])
         )
-        merged_df = merged_df.drop(['categoria_ajustada_brick', 'categoria_ajustada_utc', 'categoria_ajustada_x', 'categoria_ajustada_y'], axis=1)
+        merged_df = merged_df.drop(
+            [
+                "categoria_ajustada_brick",
+                "categoria_ajustada_utc",
+                "categoria_ajustada_x",
+                "categoria_ajustada_y",
+            ],
+            axis=1,
+        )
+        merged_df["pareto_classification_brick"] = (
+            df_brick_categoria_filtered["pareto_classification"]
+            )
+        merged_df["pareto_classification_utc"] = (
+            df_utc_categoria_filtered["pareto_classification"]
+            )
+        merged_df["pareto_classification_uf_tam"] = (
+            df_classe_categoria["pareto_classification"]
+            )
 
-
+    
     except Exception as e:
         st.error(f"Erro ao combinar DataFrames: {e}")
-    
-    st.dataframe(merged_df)
+
+    progress.progress(8 / total_etapas)
+    result = merged_df.merge(df_sellout_julho, left_on='ean', right_on='cod_barras', how='inner')
+
+    # Imprimir o número total de linhas após o join
+    st.write(f"Número total de linhas após o join: {len(result)}")
 
 
-    # saida da classificacao de pareto
+
+            
+    brick_filter_list = ["A", "B"]
+    utc_filter_list = ["A", "B"]
+    uftam_filter_list = ["A", "B"] 
+
+    #func para definir se recomenda o produto se ele for A ou em tanto em iqivia, close-up ou tamanho/regiao
+
+    def recomendar_func(row, brick_filter_list, utc_filter_list, uftam_filter_list):
+        if (row['pareto_classification_brick'] in brick_filter_list or
+            row['pareto_classification_utc'] in utc_filter_list or
+            row['pareto_classification_uf_tam'] in uftam_filter_list):
+            return 'Sim'
+        else:
+            return 'Não'
+
+
 
     try:
         df_out = pd.DataFrame(
@@ -354,32 +363,27 @@ if st.button("Processamento dos dados"):
                 "grupo do produto",
                 "pareto_classification_brick",
                 "pareto_classification_utc",
-                "pareto_classification_classe",
+                "pareto_classification_uf_tam", 
+                "Recomendar",  
             ]
-        ) 
+        )
 
         df_out["ean"] = merged_df["ean"]
         df_out["medicamentos"] = merged_df["produto"]
         df_out["grupo do produto"] = merged_df["categoria_ajustada"]
-        df_out[
-            [
-                "pareto_classification_brick",
-                "pareto_classification_utc",
-                "pareto_classification_classe",
-                    ]
-            ] = merged_df[
-                [
-                "pareto_classification_brick",
-                "pareto_classification_utc",
-                "pareto_classification",
-                ]
+        df_out[["pareto_classification_brick", "pareto_classification_utc", "pareto_classification_uf_tam"]] = merged_df[["pareto_classification_brick", "pareto_classification_utc", "pareto_classification_uf_tam"]]
+        df_out['Recomendar'] = df_out.apply(lambda row: recomendar_func(row, brick_filter_list, utc_filter_list, uftam_filter_list), axis=1)
 
-        ]
+        st.dataframe(df_out)
 
     except Exception as e:
         st.error(f"Erro ao adicionar classificações de Pareto: {e}")
 
-    st.write("Processo concluído com sucesso!")
+    progress.progress(9 / total_etapas)
+
+    st.write("Processamento concluído!")
+    progress.empty()
+
     csv = df_out.to_csv(index=False)
 
     # botao para download
@@ -389,9 +393,12 @@ if st.button("Processamento dos dados"):
         file_name="dados_processados.csv",
         mime="text/csv",
     )
+st.markdown('</div>', unsafe_allow_html=True)
 
-#colocar nome do produto, e puxar dado do "cadastro do produto"
-#coluna de categoria ajustada no df_out
+
+
+# colocar nome do produto, e puxar dado do "cadastro do produto"
+# coluna de categoria ajustada no df_out
 # Ordem das colunas:
 # Código de EAN
 # nome do produto
@@ -400,13 +407,15 @@ if st.button("Processamento dos dados"):
 # Pareto classificação utc
 # Pareto classificação classe
 
-st.write("")  
-st.write("")  
-st.write("") 
-st.write("") 
-st.write("")  
+st.write("")
+st.write("")
+st.write("")
+st.write("")
+st.write("")
 
-st.image("cogfar.png", width=300, use_column_width='never', caption="", output_format='auto')
+st.image(
+    "farm.png", width=150, use_column_width="never", caption="", output_format="auto"
+)
 
 st.markdown(
     """
@@ -418,5 +427,8 @@ st.markdown(
     }
     </style>
     """,
-    unsafe_allow_html=True
+    unsafe_allow_html=True,
 )
+
+
+# pegar a base de vendas, G, SUL e fazer o pareto desse cara no excel, fazer um pareto por categoria, fazer uma soma
