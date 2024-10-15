f27390f3bbeb0bc94d440ad4afa27cd22cc72b42
Pedro Henrique Galhardi <pedro.galhardi@cognitivo.ai>
2024-10-15 15:32:58 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -136,8 +136,8 @@ if st.button("Processamento dos dados"):
 
     # conectando com a AWS
     session = boto3.Session(
-        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+        aws_access_key_id=st.secrets["AWS"]("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=st.secrets["AWS"]("AWS_SECRET_ACCESS_KEY"),
         region_name="us-east-2",
     )
     s3 = session.client("s3")
