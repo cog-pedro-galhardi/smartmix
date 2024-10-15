972bd69676e63fc9ca8ba23891c9382c3b9c8485
cog-pedro-galhardi <pedro.galhardi@cognitivo.ai>
2024-10-15 14:49:40 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ import boto3
 import io
 import os
 from dotenv import load_dotenv
+load_dotenv()
+
 
 
 st.markdown(
@@ -133,7 +135,6 @@ if st.button("Processamento dos dados"):
     progress = st.progress(0)
 
     # conectando com a AWS
-    load_dotenv()
     session = boto3.Session(
         aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
         aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
